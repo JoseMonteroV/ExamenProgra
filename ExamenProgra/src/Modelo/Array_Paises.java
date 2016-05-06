1f31efa55cd615ea79d0cdb088dc6756f5318ee9
JoseMonteroV <fdo2494montero@gmail.com>
2016-05-06 08:53:41 -0600

    Primer aporte

--- ExamenProgra/src/Modelo/Array_Paises.java
+++ ExamenProgra/src/Modelo/Array_Paises.java
@@ -9,13 +9,6 @@ package Modelo;
  *
  * @author Jose Montero
  */
-public class ExamenProgra {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+public class Array_Paises {
     
 }
